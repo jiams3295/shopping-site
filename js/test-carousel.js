ed772a4abad08aec5ad81456ef83a2e5b32804d2
EC2 Default User <ec2-user@ip-172-31-34-68.ec2.internal>
2018-08-07 12:01:00 +0000

    Lesson9 Kadai

---
+++ js/test-carousel.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        items: 1,
+        autoplay: true
+    });
+});
+
+// ハンバーガーメニュー
+$(document).ready(function() {
+    $("#sp_menu_btn").click(function() {
+        $(".toggle").slideToggle();
+    });
+});
+
+// ギャラリーの画像切替
+$(window).load(function() {
+    $("#navi img").click(function() {
+        var img_src = $(this).attr("src");
+        $("#display_photo>img").attr("src", img_src);
+        return false;
+    });
+});
